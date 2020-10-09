f3619e0af5f94af909230a6be51f717e925e94ea
zhonganping <mysticzap@qq.com>
2020-10-09 15:45:20 +0800

    增加字符串和数据帮助类

---
+++ src/ArrayHelper.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+namespace innln\comm;
+
+/**
+ * 数据帮助类
+ * 
+ */
+class ArrayHelper
+{
+    /**
+     * 测试
+     */
+    public static function test(): string {
+        return 'test success.';
+    }
+}
---
+++ src/StringHelper.php
@@ -0,0 +1,13 @@
+<?php
+declear(strict_types=1);
+namespace innln\comm;
+
+/**
+ * 字符串帮助类
+ * 
+ */
+class StringHelper {
+    public static function test(): string {
+        return 'test StringHepler class success.';
+    }
+}
